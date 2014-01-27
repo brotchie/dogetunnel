bee4ab30c17cf4a95af371f783d72857e80e9f23
James Brotchie <brotchie@gmail.com>
2014-01-27 21:23:47 +1000

    Updated simulator and added basic test script.

--- dogecoin-sim/simulator.js
+++ dogecoin-sim/simulator.js
@@ -2,6 +2,7 @@
 
 /* Parse command line options */
 var optimist = require('optimist')
+     .usage('$0 test_script')
      .options('h', {
        alias: 'help',
        description: 'Show help'
@@ -13,17 +14,46 @@ var optimist = require('optimist')
      })
   , argv = optimist.argv;
 
-if (argv.help) {
+if (argv.help || argv._.length != 1) {
   optimist.showHelp();
   process.exit(0);
 }
 
-var jayson = require('jayson');
+var fs = require('fs')
+  , test_script_path = argv._[0]
+  , test_script = JSON.parse(fs.readFileSync(test_script_path));
+
+var jayson = require('jayson')
+  , _ = require('lodash');
+
+function simulate(name) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    var callback = args.pop();
+
+    var expected = test_script.shift();
+
+    if (name !== expected.method) {
+      console.error('expected', expected.method, JSON.stringify(expected.params));
+      process.exit(1);
+    }
+    if (expected.params && !_.isEqual(args, expected.params)) {
+      console.error('expected ' + JSON.stringify(expected.params));
+      process.exit(1);
+    }
+    callback(null, expected.result);
+    if (test_script.length == 0) {
+      setTimeout(function() {
+        process.exit(0);
+      }, 100);
+    }
+  }
+}
 
 var server = jayson.server({
-  getbalance: function(callback) {
-    callback(null, 1234);
-  }
+  getnewaddress: simulate('getnewaddress'),
+  listunspent: simulate('listunspent'),
+  gettransaction: simulate('gettransaction')
 }, {
   version: 1
 });
