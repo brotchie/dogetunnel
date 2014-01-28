2154cd1f83ab03ded99caa195a779e3302253cd4
James Brotchie <brotchie@gmail.com>
2014-01-28 17:21:31 +1000

    Completed first system test.

--- systest/test/test.system.js
+++ systest/test/test.system.js
@@ -11,6 +11,8 @@ var Processor = require('processing/processor')
 var TEST_DB_STRING = 'postgres://dogetunnel@/dogetunneltest'
   , TEST_DOGECOIND_PORT = 454546;
 
+var DEFAULT_PUBLIC_ADDRESS = 'DL4TqXtbE3iAS49qQgkV2iWWuP6h4HyMTC';
+
 var dogecoin = require('node-dogecoin')({
   user: 'dogecoin',
   pass: 'dogecoin',
@@ -36,7 +38,7 @@ describe('System Tests', function() {
         self.client.query('TRUNCATE account * CASCADE;', next);
       },
       function(_, next) {
-        self.client.query('INSERT INTO account (public_address, password_hash) VALUES ($1, $2)', ['DL4TqXtbE3iAS49qQgkV2iWWuP6h4HyMTC', 'password'], next);
+        self.client.query('INSERT INTO account (public_address, password_hash) VALUES ($1, $2)', [DEFAULT_PUBLIC_ADDRESS, 'password'], next);
       },
       function(_, next) {
         self.simulator.listen(TEST_DOGECOIND_PORT, next);
@@ -49,17 +51,29 @@ describe('System Tests', function() {
     this.simulator.close();
   });
 
-  it('should do this', function(done) {
-    var processor = this.processor;
+  it('should handle a complete transaction flow', function(done) {
+    var processor = this.processor
+      , client = this.client;
+
+    var txid = '49af0dba3d4e7d88c917493765ebf90355f7e092fe544ce489fbef709fa2a89c';
     this.simulator.setScript('scripts/processor-flow.json');
 
-    async.series([
+    async.waterfall([
       function(next) {
         processor.process(next);
       },
       function(next) {
         processor.process(next);
+      },
+      function(next) {
+        client.query('SELECT * FROM transaction WHERE public_address=$1 AND txid=$2;', [DEFAULT_PUBLIC_ADDRESS, txid], next); 
+      },
+      function(data, next) {
+        var tx = data.rows[0];
+        assert.equal(tx.state, 'confirmed');
+        assert.equal(tx.confirmations, 4);
+        next();
       }
-    ], done); 
+    ], done);
   });
 });
